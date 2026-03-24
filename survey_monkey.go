c1e61b75570ffc8ee5f058c4515c9c5e2094fe7d
ejakait <ejakaitcaleb@gmail.com>
2026-03-24 16:56:48 +0300

    Refactor survey monkey mapping to FHIR
    Moves the Survey Monkey to FHIR mapping logic into its own function and updates
    the main function to read the JSON data and call the new mapping function. This
    also includes struct definitions for Survey Monkey data.

--- survey_monkey.go
+++ survey_monkey.go
@@ -7,56 +7,13 @@ import (
 	htmlsanitizer "github.com/microcosm-cc/bluemonday"
 )
 
-type QuestionFamily string
-
-const (
-	SingleChoice QuestionFamily = "single_choice"
-	Matrix       QuestionFamily = "matrix"
-)
-
-func (qf QuestionFamily) String() string {
-	return string(qf)
-}
-
-type Questions struct {
-	id      string
-	answers []Answers
-	family  QuestionFamily
-	subtype string
-	heading string
-}
-type Answers struct {
-	choice_id   string
-	row_id      string
-	simple_text string
-}
-type Pages struct {
-	id        string
-	questions []Questions
-}
-type Responses struct {
-	id              string
-	recipient_id    string
-	response_status string
-	first_name      string
-	last_name       string
-	email_address   string
-	survey_id       string
-	collector_id    string
-	language        string
-	date_created    string
-	date_modified   string
-	analyze_url     string
-	pages           []Pages
-}
-
-func (a Answers) SeparateSimpleTextResponse() ([]string, error) {
+func SeparateSimpleTextResponse(a Answers) ([]string, error) {
 
 	var questionAnswers []string
-	questionAnswers = strings.Split(a.simple_text, "|")
+	questionAnswers = strings.Split(a.SimpleText, "|")
 
 	if len(questionAnswers) > 2 {
-		return questionAnswers, fmt.Errorf("more than 2 sections were found in the simple text: %s", a.simple_text)
+		return questionAnswers, fmt.Errorf("more than 2 sections were found in the simple text: %s", a.SimpleText)
 
 	}
 	strippedQuestionAnswers := make([]string, 0, len(questionAnswers))
@@ -67,8 +24,15 @@ func (a Answers) SeparateSimpleTextResponse() ([]string, error) {
 	return strippedQuestionAnswers, nil
 }
 
-func (q Questions) RemoveHTMLTags() string {
+func RemoveHTMLTags(target string) string {
 	sanitizer := htmlsanitizer.StripTagsPolicy()
-	sanitized := strings.TrimSpace(sanitizer.Sanitize(q.heading))
-	return sanitized
+	target = strings.TrimSpace(sanitizer.Sanitize(target))
+	r := strings.NewReplacer(
+		`\u003cbr\u003e`, "\n",
+		`\u003c`, "<",
+		`\u003e`, ">",
+		`\u0026`, "&",
+	)
+	target = r.Replace(target)
+	return target
 }
